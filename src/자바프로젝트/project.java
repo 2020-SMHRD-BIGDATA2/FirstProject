f089b8733a3f433b7794a7e203df1281fea56343
SHMRD <SHMRD@DESKTOP-F34LN0M>
2020-06-19 16:11:08 +0900

    as

--- src/자바프로젝트/project.java
+++ src/자바프로젝트/project.java
@@ -2,11 +2,11 @@ package 자바프로젝트;
 
 public class project {
 
-public static void main(String[] args) {
-	
-	System.out.println("환영합니다");  
-	System.out.println("환영해요");
-	
-	System.out.println("dfasd");
-}
+	public static void main(String[] args) {
+
+		System.out.println("환영합니다");
+		System.out.println("환영해요");
+
+		System.out.println("dfasd");
+	}
 }
